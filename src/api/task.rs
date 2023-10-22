80a2860a676a9ccc3bb30ed0b5f923076d964648
HeinzDev <heinz1dev@gmail.com>
2023-10-22 19:15:19 -0300

    implemented task and fixed the main.rs

--- src/api/task.rs
+++ src/api/task.rs
@@ -1,3 +1,6 @@
+use crate::model::task::Task;
+use crate::model::task::TaskState;
+use crate::repository::ddb::DDBRepository;
 use actix_web::{
     error::ResponseError,
     get,
@@ -9,14 +12,61 @@ use actix_web::{
     HttpResponse,
 };
 use derive_more::Display;
-use serve::{Deserialize, Serialize};
+use serde::{Deserialize, Serialize};
 
 #[derive(Deserialize, Serialize)]
 pub struct TaskIdentifier {
     taks_global_id: String,
 }
 
+#[derive(Deserialize)]
+pub struct TaskCompletionRequest {
+    result_file: String,
+}
+
+#[derive(Deserialize)]
+pub struct SubmitTaskRequest {
+    user_id: String,
+    task_typep: String,
+    source_file: String,
+}
+
+#[derive(Debug, Display)]
+pub enum TaskError {
+    TaskNotFound,
+    TaksUpdateFailure,
+    TaskCreationFailure,
+    BadTaskRequest,
+}
+
+impl ResponseError for TaskError {
+    fn error_response(&self) -> HttpResponse {
+        HttpResponse::build(self.status_code())
+            .insert_header(ContentType::json())
+            .body(self.to_string())
+    }
+
+    fn status_code(&self) -> StatusCode {
+        match self {
+            TaskError::TaskNotFound => StatusCode::NOT_FOUND,
+            TaskError::TaksUpdateFailure => StatusCode::FAILED_DEPENDENCY,
+            TaskError::TaskCreationFailure => StatusCode::FAILED_DEPENDENCY,
+            TaskError::BadTaskRequest => StatusCode::BAD_REQUEST,
+        }
+    }
+}
+
 #[get("/task/{task_global_id}")]
-pub async fn get_task(task_identifier: Path<TaskIdentifier>, body: Json<Struct>) -> Json<String> {
-    return Json(task_identifier.into_inner().taks_global_id);
+pub async fn get_task(
+    task_identifier: Path<TaskIdentifier>,
+    ddb_repo: Data<DDBRepository>,
+) -> Result<Json<Task>, TaskError> {
+    task = ddb_repo
+        .get_task(task_identifier.into_inner().taks_global_id)
+        .await;
+
+    match task {
+        Some(task) => Ok(Json(task)),
+        None => Err(TaskError::TaskNotFound),
+    }
 }
